ba27cd3df1f6b6a34c8802465bfdba00937f5723
unknown <manojbk488@gmail.com>
2023-01-07 16:47:21 +0545

    showing different content based on device orientation

--- lib/main.dart
+++ lib/main.dart
@@ -117,6 +117,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final isLandscape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
     final appBar = AppBar(
       title: Text('Personal Expenses App'),
       actions: [
@@ -128,39 +130,59 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ],
     );
+
+    final transListWidegt = Container(
+      height: (MediaQuery.of(context).size.height -
+              appBar.preferredSize.height -
+              MediaQuery.of(context).padding.top) *
+          0.7,
+      child: TransactionList(
+          transactions: _userTransactions, deleteTrans: _deleteTransaction),
+    );
     return Scaffold(
       appBar: appBar,
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Show Chart'),
-                Switch(value: _showChart, onChanged: (value){
-                  setState(() {
-                    _showChart = value;
-                  });
-                }),
-              ],
-            ),
-            _showChart ? Container(
-              height: (MediaQuery.of(context).size.height -
-                      appBar.preferredSize.height -
-                      MediaQuery.of(context).padding.top) *
-                  0.7,
-              child: Chart(recentTransaction: _recentTransactions),
-            )
-            : Container(
-              height: (MediaQuery.of(context).size.height -
-                      appBar.preferredSize.height -
-                      MediaQuery.of(context).padding.top) *
-                  0.7,
-              child: TransactionList(
-                  transactions: _userTransactions,
-                  deleteTrans: _deleteTransaction),
-            ),
+            if (isLandscape)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Show Chart'),
+                  Switch(
+                    value: _showChart,
+                    onChanged: (value) {
+                      setState(
+                        () {
+                          _showChart = value;
+                        },
+                      );
+                    },
+                  ),
+                ],
+              ),
+              if(!isLandscape)
+              Container(
+                height: (MediaQuery.of(context).size.height -
+                        appBar.preferredSize.height -
+                        MediaQuery.of(context).padding.top) *
+                    0.3,
+                child: Chart(recentTransaction: _recentTransactions),
+              ),
+              if(!isLandscape)
+              transListWidegt,
+
+            if(isLandscape)
+            _showChart
+                ? Container(
+                    height: (MediaQuery.of(context).size.height -
+                            appBar.preferredSize.height -
+                            MediaQuery.of(context).padding.top) *
+                        0.7,
+                    child: Chart(recentTransaction: _recentTransactions),
+                  )
+                : transListWidegt,
           ],
         ),
       ),
